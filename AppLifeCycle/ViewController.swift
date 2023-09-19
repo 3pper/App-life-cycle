8cfe2ef8e5ab35943e2783cec618ccbef34f4bf3
Egor Denisenko <lodpsaf@gmail.com>
2023-09-19 17:36:41 +0300

    upload

---
+++ AppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Egor on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
